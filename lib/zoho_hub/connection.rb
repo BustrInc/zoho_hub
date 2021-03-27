2ffd6b4f61569ed48af656fe13d70f0161ccbb57
Ben Cwik <bcwik9@gmail.com>
2021-03-27 00:52:37 +0000

    missing bracket

--- lib/zoho_hub/connection.rb
+++ lib/zoho_hub/connection.rb
@@ -52,7 +52,7 @@ module ZohoHub
     end
 
     def put(path, params = {})
-      p = {'JSONString': params[:data].first.transform_keys!{|k| k.to_s.downcase }
+      p = {'JSONString': params[:data].first.transform_keys!{|k| k.to_s.downcase }}
       log "PUT #{path} with #{p}"
 
       response = with_refresh { adapter.put(path, p) }
